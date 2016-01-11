65554fd3fa4e0c8c65ad488afb87d5cc102cf0fa
Édouard Lopez <dev+git@edouard-lopez.com>
2016-01-11 18:21:05 +0100

    take full page screenshots

--- tests/screenshot.js
+++ tests/screenshot.js
@@ -4,7 +4,7 @@ const pageres = new Pageres({
     delay: 2,
     filename: '<%= date %>-<%= time %>.<%= url %>-<%= size %><%= crop %>'
 })
-    .src('0.0.0.0:5000', ['w3counter'], {crop: true})
+    .src('0.0.0.0:5000', ['w3counter'])
     .dest(__dirname + '/screenshots')
     .run()
     .then(function () {
